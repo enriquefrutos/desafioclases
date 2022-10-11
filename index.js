878237a8f8d6be804a4ff910f8ccb9f7f9470daa
enriquefrutos <90933657+enriquefrutos@users.noreply.github.com>
2022-10-11 15:30:51 -0300

    nuevo

---
+++ index.js
@@ -0,0 +1,21 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascotas){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = [];
+        this.mascotas = [];
+    }
+    getFullName(){console.log(`El nombre es ${this.nombre} ${this.apellido}`)};
+    addMascota(mascota){this.mascotas.push(mascota);}
+    countMascotas(){console.log(this.mascotas.length);}
+    addBook(nombre,autor){this.libros.push({nombre:nombre,autor:autor});}
+    getBookNames(){console.log(this.libros.forEach((libro) =>{console.log(libro)}))}
+    }
+
+const obj = new Usuario ('enrique','frutos')
+    obj.getFullName();
+    obj.addMascota('lucas');
+    obj.addMascota('lucas');
+    obj.countMascotas()
+    obj.addBook('pepe','juan')
+    obj.getBookNames()
